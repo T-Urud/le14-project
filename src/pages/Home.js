aa37d4d11c7ebfb5ce5d72c72f5a178120eb396f
urud.theo@gmail.com <urud.theo@gmail.com>
2024-08-27 16:58:00 +0200

    disposition update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,38 @@
 import React from "react";
 import Header from "../components/Header";
+import backgroundImg from "../assets/landscape_art_road_127350_800x600.jpg";
+import Activity from "../components/Activity";
 
 const Home = () => {
   return (
-    <div>
+    <section className="flex">
+      <Activity />
       <Header />
-      <h1>Duplex Le14</h1>
-      <h2>Maison de 120m2 en plein centre de Tain l'Hermitage</h2>
-      <div className="grid">
-        <div className="bg-slate-400 rounded-md">
-          <h2>Capacité</h2>
-          <span>4 personnes</span>
-        </div>
-        <div className="">
-          <h2>120m2</h2>
-          <span>4 personnes</span>
-        </div>
-        <div className="bg-slate-200 rounded-md">
-          <h2>120m2</h2>
-          <span>4 personnes</span>
-        </div>
-        <div className="">
-          <h2>120m2</h2>
-          <span>4 personnes</span>
+      <div className="w-1/2">
+        <h1>Duplex Le14</h1>
+        <h2>Maison de 120m2 en plein centre de Tain l'Hermitage</h2>
+        <div className="grid grid-cols-2 my-8">
+          <div className="bg-slate-400 rounded-md w-48 h-20">
+            <h2>Capacité</h2>
+            <span>4 personnes</span>
+          </div>
+          <div className="bg-slate-200 rounded-md w-48 h-20">
+            <h2>120m2</h2>
+            <span>4 personnes</span>
+          </div>
+          <div
+            className="rounded-lg w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage: `url(${backgroundImg})`,
+            }}
+          ></div>
+          <div className="">
+            <h2>120m2</h2>
+            <span>4 personnes</span>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
