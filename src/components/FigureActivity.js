1e0c06124a604a055d9389c50114cc1ee845bf58
urud.theo@gmail.com <urud.theo@gmail.com>
2024-09-01 13:57:12 +0200

    animation update on activity cards, no more framer motion for this part

--- src/components/FigureActivity.js
+++ src/components/FigureActivity.js
@@ -2,45 +2,32 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const FigureActivity = ({ activity }) => {
-  // const [focus, setFocus] = useState(false);
-  // const handleCardFocus = () => {
-  //   setFocus(true);
-  // };
-  // const handleCardBlur = () => {
-  //   setFocus(false);
-  // };
-
   return (
     <li className="w-1/3">
-      <motion.figure
-        // onFocus={handleCardFocus}
-        // onBlur={handleCardBlur}
-        whileHover={{ opacity: 1 }}
-        className="w-full relative flex items-center justify-center overflow-hidden gradient-overlay rounded-xl"
+      <a
+        href={activity.link}
+        target="blank"
+        rel="noopener noreferrer"
+        className="group"
       >
-        <img
-          src={activity.picture}
-          alt={activity.description}
-          className="w-full h-full bg-cover bg-center"
-        />
-        <div className="inset-0 absolute flex justify-end items-center flex-col z-10 py-10">
-          <h3 className="text-xl italic bottom-1 text-white">
-            {activity.title}
-          </h3>
-        </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileHover={{ opacity: 1 }}
-          // transition={{ duration: 0.3 }}
-          className="absolute inset-0"
-        >
-          <p className="text-3xl italic bottom-1 text-white z-10">
-            <a href={activity.link} target="blank" rel="noopener noreferrer">
-              +
-            </a>
-          </p>
-        </motion.div>
-      </motion.figure>
+        <figure className="w-full relative flex items-center justify-center overflow-hidden gradient-overlay rounded-xl">
+          <img
+            src={activity.picture}
+            alt={activity.description}
+            className="w-full h-full bg-cover bg-center"
+          />
+          <div className="inset-0 absolute flex justify-end items-center flex-col z-10 pt-10">
+            <h3 className="text-xl italic bottom-1 text-white">
+              {activity.title}
+            </h3>
+            <div className="z-10 cursor-pointer w-full flex items-center justify-center pb-2 opacity-0 group-hover:opacity-100 duration-500">
+              <button className="w-full flex items-center justify-center z-10 text-white text-3xl">
+                +
+              </button>
+            </div>
+          </div>
+        </figure>
+      </a>
     </li>
   );
 };
